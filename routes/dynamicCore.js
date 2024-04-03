d8a75304ebaf9e320696c62fac72641ea2209581
LAPTOP-8I3EL5BD\lenovo <mothership2002@naver.com>
2024-04-03 15:50:00 +0900

    no message

---
+++ routes/dynamicCore.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const resourceObject = {};
+
+const createResource = (body) => {
+
+};
+
+const readResource = (body) => {
+
+};
+
+const updateResource = (body) => {
+
+};
+
+const deleteResource = (body) => {
+
+};
+
+module.exports = {
+    createResource,
+    readResource,
+    updateResource,
+    deleteResource
+}
---
+++ routes/resourceRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const dynamicCore = require('./dynamicCore');
+const extension = '.json';
+
+// pagination?
+router.get('/all', (req, res) => {
+    res.send(); // 여기에 목록 조회 할 것.
+});
+
+router.post('/', (req, res) => {
+   req.body;
+   // 성공 및 실패 여부 res 담는 방법 찾아야함.
+});
+
+router.delete('/:filename', (req, res) => {
+    const filename = req.params.filename + extension;
+    // 대충 객체값 지우는 로직
+    // 파일도 지우는 로직
+    res.send();
+});
+
+
+
+module.exports = router
+
+
